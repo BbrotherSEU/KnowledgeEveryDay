b8ecbf3873b397ab7f20837fec7c79889dc72b90
范彬 <kevin@fanbin-3.local>
2018-08-23 18:17:30 +0800

    add maven

---
+++ src/main/java/fanbin/KnowledgeEveryDay/App.java
@@ -0,0 +1,13 @@
+package fanbin.KnowledgeEveryDay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
